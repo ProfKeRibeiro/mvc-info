6a0ec24fd45b901b52b01487d31aa6256f15ee9d
Kelly Ribeiro <kcg.r1997@gmail.com>
2026-03-13 16:15:13 -0300

    versões  projeto mvc

--- backend/server1.js
+++ backend/server1.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const eventRoutes = require("./src/routes/eventRoutes");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "src/views"));
 
-app.use("/", eventRoutes);
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/eventos", (req, res)=>{
+    res.send("Lista de Eventos")
+})
+
+
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000");
--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -2,32 +2,14 @@ const express = require("express");
 
 const router = express.Router();
 
-const eventos = [];
-
 router.get("/", (req, res) => {
     res.render("index");
 });
 
-router.get("/eventos/novo", (req, res) => {
-    res.render("new");
-});
-
 router.get("/eventos", (req, res) => {
-    res.render("event", { eventos });
+    res.send("Lista de eventos");
 });
 
-router.post("/eventos", (req, res) => {
 
-    const nome = req.body.nome;
-    const data = req.body.data;
-
-    eventos.push({
-        nome: nome,
-        data: data
-    });
-
-    res.redirect("/eventos");
-
-});
 
 module.exports = router;
